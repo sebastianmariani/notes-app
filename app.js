3a0d2d7bb2b0ef0bf1cb8c5e3f246c77e2a1612b
User <sebastian.mariani.94@gmail.com>
2020-09-02 17:25:49 +0100

    add notes

--- app.js
+++ app.js
@@ -1,12 +1,58 @@
+const fs = require('fs')
 const chalk = require('chalk')
+const yargs = require('yargs')
 const getNotes = require('./notes')
 
-const command = process.argv[2]
+//customize yargs version
+yargs.version('1.1.0')
 
-console.log(process.argv)
+//create add command
+yargs.command({
+    command: 'add',     
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        console.log('Title: ' + argv.title)
+        console.log('Body: ' + argv.body)
+    }
+})
 
-if (command === 'add') {
-    console.log('adding note')
-} else if (command === 'remove') {
-    console.log('removing note!')
-}
+//Create remove command 
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function () {
+        console.log('removing a note!')
+    }
+})
+
+//Create list command
+yargs.command({
+    command: 'list',
+    describe: 'list the notes',
+    handler: function () {
+        console.log('listing notes!')
+    }
+})
+
+//Create read command
+yargs.command({
+    command: 'read',
+    describe: 'read a note',
+    handler: function () {
+        console.log('reading a note!')
+    }
+})
+
+yargs.parse()
