cebde67c35d3aaffc7f6704427a3083fdbc03443
User <sebastian.mariani.94@gmail.com>
2020-09-03 17:00:56 +0100

    add functionality conditional

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const chalk = require('chalk')
 const yargs = require('yargs')
-const getNotes = require('./notes')
+const notes = require('./notes')
 
 //customize yargs version
 yargs.version('1.1.0')
@@ -23,10 +23,10 @@ yargs.command({
         }
     },
     handler: function (argv) {
-        console.log('Title: ' + argv.title)
-        console.log('Body: ' + argv.body)
+        notes.addNote(argv.title, argv.body)
     }
 })
+
 
 //Create remove command 
 yargs.command({
